9229af235f6e3260407ceb4710dbbff3704bc951
Puja Nakkala <pnakkala@unc.edu>
2024-08-26 15:30:10 -0400

    Finished exercise 00!

---
+++ lessons/functions.py
@@ -0,0 +1,12 @@
+"""Practice with functions"""
+
+
+# define a function
+def sum(num1: int, num2: int) -> float:
+    """returns sum of 2 numbers"""
+    return float(num1 + num2)
+
+
+# call a function
+print(sum(num1=1, num2=10))
+# (sum(num1=randint(1, 10), num2=randint(2, 20)))
